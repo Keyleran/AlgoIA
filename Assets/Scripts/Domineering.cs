7ff6102b89a5ddcbed7f88a05773d1f14ff9ec63
Kevin Rey <k.rey@hotmail.fr>
2017-03-13 16:59:22 +0100

    First Push Nim Game Domineering MiniMax

---
+++ Assets/Scripts/Domineering.cs
@@ -0,0 +1,427 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Domineering : MonoBehaviour
+{
+    [SerializeField]
+    int _width = 5;
+
+    [SerializeField]
+    int _heigth = 5;
+
+    [SerializeField]
+    Material[] _materials;
+
+    [SerializeField]
+    bool _minimax = true;
+
+    [SerializeField]
+    int _recursivity = 5;
+
+    private GameObject[][] _quadrillage;
+    private bool _playerTurn = true;
+    private bool[][] _quadrillageState;
+
+    private int _posCursorX = 0;
+    private int _posCursorY = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        bool pairWidth = _width % 2 == 0 ? true : false;
+        bool pairHeigth = _heigth % 2 == 0 ? true : false;
+
+        float posX = (-_width / 2) + (pairWidth  ? 0.5f : 0);
+        float posY = (_heigth / 2) - (pairHeigth ? 0.5f : 0);
+
+        _quadrillage = new GameObject[_heigth][];
+        _quadrillageState = new bool[_heigth][];
+        for (int i = 0; i < _heigth; i++)
+        {
+            _quadrillage[i] = new GameObject[_width];
+            _quadrillageState[i] = new bool[_width];
+            for (int j = 0; j < _width; j++)
+            {
+                _quadrillage[i][j] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _quadrillage[i][j].transform.position = new Vector3(posX, posY, 0);
+                _quadrillageState[i][j] = false;
+                posX++;
+            }
+            posX = (-_width / 2) + (pairWidth ? 0.5f : 0);
+            posY--;
+        }
+
+        ChangeToPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerTurn)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ResetHighlight();
+                _posCursorX = 0;
+
+                int lineY = _posCursorY - 1;
+                int colX = 0;
+                while (lineY > -1)
+                {
+                    colX = GetPosXFree(lineY);
+                    if (colX == -1)
+                        lineY--;
+                    else
+                    {
+                        _posCursorX = colX;
+                        _posCursorY = lineY;
+                        break;
+                    }
+                }
+                HighlightPlayerPick(_posCursorX, _posCursorY);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                ResetHighlight();
+                _posCursorX = 0;
+
+                int lineY = _posCursorY + 1;
+                int colX = 0;
+                while (lineY < _heigth)
+                {
+                    colX = GetPosXFree(lineY);
+                    if (colX == -1)
+                        lineY++;
+                    else
+                    {
+                        _posCursorX = colX;
+                        _posCursorY = lineY;
+                        break;
+                    }
+                }
+                HighlightPlayerPick(_posCursorX, _posCursorY);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                ResetHighlight();
+                int posX = GetNextPosXFree(_posCursorY, _posCursorX, -1);
+
+                if (posX != -1)
+                    _posCursorX = posX;
+                HighlightPlayerPick(_posCursorX, _posCursorY);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ResetHighlight();
+                int posX = GetNextPosXFree(_posCursorY, _posCursorX, 1);
+
+                if (posX != -1)
+                    _posCursorX = posX;
+                HighlightPlayerPick(_posCursorX, _posCursorY);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SetPlayerPick(_posCursorX, _posCursorY);
+                _playerTurn = false;
+                ChangeToIA();
+            }
+        }
+    }
+
+    void ChangeToPlayer()
+    {
+        _posCursorX = -1;
+        _posCursorY = -1;
+        ResetHighlight();
+
+        int lineY = 0;
+        while (lineY < _heigth)
+        {
+            int colX = 0;
+            while (colX < _width - 1)
+            {
+                if(_quadrillageState[lineY][colX] == false && _quadrillageState[lineY][colX + 1] == false)
+                {
+                    _posCursorX = colX;
+                    _posCursorY = lineY;
+                }
+
+                if (_posCursorX >= 0 && _posCursorY >= 0)
+                    break;
+
+                colX++;
+            }
+
+            if (_posCursorX >= 0 && _posCursorY >= 0)
+                break;
+
+            lineY++;
+        }
+
+        if (_posCursorX >= 0 && _posCursorY >= 0)
+        {
+            HighlightPlayerPick(_posCursorX, _posCursorY);
+            _playerTurn = true;
+        }
+        else
+        {
+            Debug.Log("Player Game Over");
+        }
+    }
+
+
+    void ChangeToIA()
+    {
+        _posCursorX = -1;
+        _posCursorY = -1;
+
+        if(!_minimax)
+        {
+            int heuristic = -100;
+
+            int lineY = 0;
+            while (lineY < _heigth - 1)
+            {
+                int colX = 0;
+                while (colX < _width)
+                {
+                    if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY + 1][colX] == false)
+                    {
+                        _quadrillageState[lineY][colX] = true;
+                        _quadrillageState[lineY + 1][colX] = true;
+
+                        int tempHeur = NbSoluceIA() - NbSolucePlayer();
+                        if (tempHeur > heuristic)
+                        {
+                            heuristic = tempHeur;
+                            _posCursorX = colX;
+                            _posCursorY = lineY;
+                        }
+
+                        _quadrillageState[lineY][colX] = false;
+                        _quadrillageState[lineY + 1][colX] = false;
+                    }
+                    colX++;
+                }
+                lineY++;
+            }
+        }
+        else
+        {
+            MiniMax(_recursivity, 0);
+        }
+
+        if (_posCursorX >= 0 && _posCursorY >= 0)
+        {
+            SetIAPick(_posCursorX, _posCursorY);
+            ChangeToPlayer();
+        }
+        else
+        {
+            Debug.Log("IA Game Over");
+        }
+    }
+
+    void ResetHighlight()
+    {
+        for (int j = 0; j < _heigth; j++)
+        {
+            for (int i = 0; i < _width; i++)
+            {
+                if(_quadrillageState[j][i] == false)
+                    _quadrillage[j][i].GetComponent<Renderer>().material = new Material(_materials[0]);
+            }
+        }
+    }
+
+    void HighlightPlayerPick(int x, int y)
+    {
+        _quadrillage[y][x].GetComponent<Renderer>().material = new Material(_materials[1]);
+        _quadrillage[y][x + 1].GetComponent<Renderer>().material = new Material(_materials[1]);
+    }
+
+    void SetPlayerPick(int x, int y)
+    {
+        _quadrillage[y][x].GetComponent<Renderer>().material = new Material(_materials[3]);
+        _quadrillage[y][x + 1].GetComponent<Renderer>().material = new Material(_materials[3]);
+
+        _quadrillageState[y][x] = true;
+        _quadrillageState[y][x + 1] = true;
+    }
+
+    void SetIAPick(int x, int y)
+    {
+        _quadrillage[y][x].GetComponent<Renderer>().material = new Material(_materials[2]);
+        _quadrillage[y + 1][x].GetComponent<Renderer>().material = new Material(_materials[2]);
+
+        _quadrillageState[y][x] = true;
+        _quadrillageState[y + 1][x] = true;
+    }
+
+
+    int GetPosXFree(int lineY)
+    {
+        int posX = -1;
+
+        int colX = 0;
+        while (colX < _width - 1)
+        {
+            if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY][colX + 1] == false)
+            {
+                posX = colX;
+            }
+
+            if (posX >= 0)
+                break;
+
+            colX++;
+        }
+
+        return posX;
+    }
+
+    int GetNextPosXFree(int lineY, int colX, int offset)
+    {
+        int posX = -1;
+
+        colX += offset;
+        while ((offset == 1 && colX < _width - 1) || (offset == -1 && colX > - 1))
+        {
+            if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY][colX + 1] == false)
+            {
+                posX = colX;
+            }
+
+            if (posX >= 0)
+                break;
+
+            colX += offset;
+        }
+
+        return posX;
+    }
+
+
+    int NbSolucePlayer()
+    {
+        int nbSoluce = 0;
+
+        int lineY = 0;
+        while (lineY < _heigth)
+        {
+            int colX = 0;
+            while (colX < _width - 1)
+            {
+                if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY][colX + 1] == false)
+                {
+                    nbSoluce++;
+                }
+                colX++;
+            }
+            lineY++;
+        }
+        return nbSoluce;
+    }
+
+    int NbSoluceIA()
+    {
+        int nbSoluce = 0;
+
+        int lineY = 0;
+        while (lineY < _heigth - 1)
+        {
+            int colX = 0;
+            while (colX < _width)
+            {
+                if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY + 1][colX] == false)
+                {
+                    nbSoluce++;
+                }
+                colX++;
+            }
+            lineY++;
+        }
+        return nbSoluce;
+    }
+
+    int MiniMax(int depth, int nbDepth)
+    {
+        bool iaTurn = nbDepth % 2 == 0 ? true : false;
+
+        if(depth == 0)
+            return NbSoluceIA() - NbSolucePlayer();
+
+        int finalHeur = 0;
+        if(iaTurn)
+        {
+            // Max
+            int heuristic = -100;
+
+            int lineY = 0;
+            while (lineY < _heigth - 1)
+            {
+                int colX = 0;
+                while (colX < _width)
+                {
+                    if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY + 1][colX] == false)
+                    {
+                        _quadrillageState[lineY][colX] = true;
+                        _quadrillageState[lineY + 1][colX] = true;
+
+                        int tempHeur = MiniMax(depth - 1, nbDepth + 1);
+                        if (tempHeur > heuristic)
+                        {
+                            heuristic = tempHeur;
+                            finalHeur = tempHeur;
+
+                            if(nbDepth == 0)
+                            {
+                                _posCursorX = colX;
+                                _posCursorY = lineY;
+                            }
+                        }
+
+                        _quadrillageState[lineY][colX] = false;
+                        _quadrillageState[lineY + 1][colX] = false;
+                    }
+                    colX++;
+                }
+                lineY++;
+            }
+        }
+        else
+        {
+            // Min
+            int heuristic = 100;
+
+            int lineY = 0;
+            while (lineY < _heigth)
+            {
+                int colX = 0;
+                while (colX < _width - 1)
+                {
+                    if (_quadrillageState[lineY][colX] == false && _quadrillageState[lineY][colX + 1] == false)
+                    {
+                        _quadrillageState[lineY][colX] = true;
+                        _quadrillageState[lineY][colX + 1] = true;
+
+                        int tempHeur = MiniMax(depth - 1, nbDepth + 1);
+                        if (tempHeur < heuristic)
+                        {
+                            heuristic = tempHeur;
+                            finalHeur = tempHeur;
+                        }
+
+                        _quadrillageState[lineY][colX] = false;
+                        _quadrillageState[lineY][colX + 1] = false;
+                    }
+                    colX++;
+                }
+                lineY++;
+            }
+        }
+        return finalHeur;
+    }
+}
---
+++ Assets/Scripts/PickManager.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] _line1;
+
+    [SerializeField]
+    GameObject[] _line2;
+
+    [SerializeField]
+    GameObject[] _line3;
+
+    [SerializeField]
+    GameObject[] _line4;
+
+    [SerializeField]
+    Material[] _materials;
+
+    private bool _playerTurn = true;
+    private int _selectLine = 0;
+    private int _selectPicks = 1;
+    private int[] _nbPick = new int[4];
+    private int[] _nbPickDelete = new int[4];
+    private GameObject[][] _allPick;
+    // Use this for initialization
+    void Start ()
+    {
+        _nbPick[0] = _line1.Length;
+        _nbPick[1] = _line2.Length;
+        _nbPick[2] = _line3.Length;
+        _nbPick[3] = _line4.Length;
+
+        _allPick = new GameObject[4][];
+        _allPick[0] = _line1;
+        _allPick[1] = _line2;
+        _allPick[2] = _line3;
+        _allPick[3] = _line4;
+
+        _nbPickDelete[0] = 0;
+        _nbPickDelete[1] = 0;
+        _nbPickDelete[2] = 0;
+        _nbPickDelete[3] = 0;
+        
+        ChangeToPlayer();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(_playerTurn)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ResetHighlight();
+                int temp = _selectLine;
+                
+                do
+                {
+                    _selectLine--;
+                    _selectPicks = 1;
+                }
+                while (_selectLine > -1 && (_nbPick[_selectLine] - _nbPickDelete[_selectLine] == 0)) ;
+
+                if (_selectLine == -1)
+                {
+                    _selectLine = temp;
+                }
+
+                HighlightLinePick(_selectLine);
+                HighlightPicks(_selectLine, _selectPicks);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                ResetHighlight();
+                int temp = _selectLine;
+
+                do
+                {
+                    _selectLine++;
+                    _selectPicks = 1;
+                }
+                while (_selectLine < 4 && (_nbPick[_selectLine] - _nbPickDelete[_selectLine] == 0));
+
+                    if (_selectLine == 4)
+                {
+                    _selectLine = temp;
+                }
+
+                HighlightLinePick(_selectLine);
+                HighlightPicks(_selectLine, _selectPicks);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                ResetHighlight();
+                if (_selectPicks > 1)
+                {
+                    _selectPicks--;
+                }
+
+                HighlightLinePick(_selectLine);
+                HighlightPicks(_selectLine, _selectPicks);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ResetHighlight();
+                if (_selectPicks < _nbPick[_selectLine] - _nbPickDelete[_selectLine])
+                {
+                    _selectPicks++;
+                }
+
+                HighlightLinePick(_selectLine);
+                HighlightPicks(_selectLine, _selectPicks);
+            }
+
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                for(int i = 0; i < _selectPicks; i++)
+                {
+                    _allPick[_selectLine][_nbPickDelete[_selectLine] + i].SetActive(false);
+                }
+                _nbPickDelete[_selectLine] += _selectPicks;
+                ResetHighlight();
+                _playerTurn = false;
+                ChangeToIA();
+            }
+        }
+	}
+
+    void ChangeToPlayer()
+    {
+        int lineId = 3;
+        _selectLine = -1;
+        ResetHighlight();
+
+        while (lineId > -1)
+        {
+            if (_nbPick[lineId] - _nbPickDelete[lineId] > 0)
+                _selectLine = lineId;
+            lineId--;
+        }
+
+        if (_selectLine >= 0)
+        {
+            _selectPicks = 1;
+            HighlightLinePick(_selectLine);
+            HighlightPicks(_selectLine, _selectPicks);
+            _playerTurn = true;
+        }
+        else
+        {
+            Debug.Log("Player Game Over");
+        }
+    }
+
+    void ChangeToIA()
+    {
+        int lineId = 3;
+        int nbPickToDelete = 1;
+        _selectLine = -1;
+        ResetHighlight();
+
+        while (lineId > -1)
+        {
+            while (_nbPick[lineId] - _nbPickDelete[lineId] - nbPickToDelete >= 0)
+            {
+                if (CheckPickXOR(lineId, nbPickToDelete) == 0)
+                {
+                    _selectLine = lineId;
+                    break;
+                }
+                else
+                    nbPickToDelete++;
+            }
+
+            if (_selectLine > -1)
+                break;
+
+            lineId--;
+            nbPickToDelete = 1;
+        }
+
+        if (_selectLine >= 0)
+        {
+            for (int i = 0; i < nbPickToDelete; i++)
+            {
+                _allPick[_selectLine][_nbPickDelete[_selectLine] + i].SetActive(false);
+            }
+            _nbPickDelete[_selectLine] += nbPickToDelete;
+            ResetHighlight();
+            ChangeToPlayer();
+        }
+        else
+        {
+            Debug.Log("IA Game Over");
+        }
+    }
+
+    void ResetHighlight()
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            for (int i = _nbPickDelete[j]; i < _nbPick[j]; i++)
+            {
+                _allPick[j][i].GetComponent<Renderer>().material = new Material(_materials[0]);
+            }
+        }
+    }
+
+    void HighlightLinePick(int lineId)
+    {
+        for(int i = _nbPickDelete[lineId]; i < _nbPick[lineId]; i++)
+        {
+            _allPick[lineId][i].GetComponent<Renderer>().material = new Material(_materials[1]);
+        }
+    }
+
+    void HighlightPicks(int lineId, int nbPick)
+    {
+        for (int i = _nbPickDelete[lineId]; i < _nbPickDelete[lineId] + nbPick; i++)
+        {
+            _allPick[lineId][i].GetComponent<Renderer>().material = new Material(_materials[2]);
+        }
+    }
+
+    int CheckPickXOR(int lineId, int nbPick)
+    {
+        int result = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            result ^= (_nbPick[i] - _nbPickDelete[i] - (i == lineId ? nbPick : 0));
+        }
+        return result;
+    }
+}
